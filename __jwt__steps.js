44d4d236b4448499b43f1748892530d529d3c183
Shishir-Ahmed <shishirahmed790@gmail.com>
2023-10-31 01:28:46 +0600

    jwt token http only cookie

---
+++ __jwt__steps.js
@@ -0,0 +1,28 @@
+/*
+1. install jsonwebtoken
+2. require jsonwebtoken
+3. jwt.sign(payload, secret, {expiresIn: })
+4. token client
+*/
+
+/*
+// how to store token in the client side
+1. memory --> not recommended but ok type
+2. local storage --> ok type
+3. cookies: http only
+*/
+
+/*
+1. set cookies with http only. for development secure: false,
+
+2. cors
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
+
+3. client side axios setting
+in axios set withCredentials: true
+*/
